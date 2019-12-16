f8bf848ecca45a05183bfd5ce4832c06834ae781
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-16 22:25:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,6 +83,11 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  arrays = []
+  index = 0
+  while index<source.length do
+    arrays.push()
+  end
   
 end
 
