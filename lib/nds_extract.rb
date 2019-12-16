33534afc1ce4bbc384b6bbee20dbaabe525ed769
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-16 21:08:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,11 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   result = []
-  result << movie_with_director_name(name,movies_collection)
+  index = 0
+  while index < movies_collection.length do
+    result << movie_with_director_name(name[index],movies_collection[index])
+    index+=1
+  end
   return result
 end
 
